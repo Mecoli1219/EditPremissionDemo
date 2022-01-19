812b34eab2b7a91c27c84ec3336450efc67b9a7c
justinhuang1111 <b09901062@ntu.edu.tw>
2022-01-19 11:54:38 +0800

    mongoose schema finished

--- final/src/models/User.js
+++ final/src/models/User.js
@@ -1,20 +1,20 @@
-import mongoose from "mongoose"
+import mongoose from "mongoose";
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 const userSchema = new Schema({
-    userID: {
-        type: String,
-        required: [true, 'userID field is required.']
-    },
-    password: {
-        type: String,
-        required: [true, 'Password field is required.']
-    },
-    name: {
-        type: String,
-        required: [true, 'Name field is required.']
-    }
-})
+  userID: {
+    type: String,
+    required: [true, "userID field is required."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password field is required."],
+  },
+  name: {
+    type: String,
+    required: [true, "Name field is required."],
+  },
+});
 
-const User = mongoose.model('User', userSchema)
-export default User
+const User = mongoose.model("User", userSchema);
+export default User;
