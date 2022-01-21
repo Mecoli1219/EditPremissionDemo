1db53d8f150717e244c5032a017d6d49842b14f2
JustinHuang1111 <b09901062@ntu.edu.tw>
2022-01-21 11:14:38 +0800

    type-graphql setup

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -4,11 +4,14 @@ import express from "express";
 import "dotenv-defaults/config";
 import http from "http";
 import bodyParser from "body-parser";
-import {ApolloServer} from "apollo-server-express"
-import {execute, subscribe} from "graphql"
+import { ApolloServer } from "apollo-server-express"
+import { execute, subscribe } from "graphql"
 import { SubscriptionServer } from 'subscriptions-transport-ws'
 import { makeExecutableSchema } from '@graphql-tools/schema'
 import { PubSub } from 'graphql-subscriptions';
+import "reflect-metadata";
+import { buildSchema } from 'type-graphql'
+import ColorResolver from './color-resolver'
 
 import resolvers from "./resolvers"
 import db from "./models"
@@ -16,65 +19,70 @@ import mongo from "./mongo"
 
 
 const port = process.env.PORT || 4000;
-const {SECRET_KEY} = process.env;
+const { SECRET_KEY } = process.env;
 
-(async function(){
-    const app = express()
-    app.use(bodyParser.json())
+(async function () {
+  const app = express()
+  app.use(bodyParser.json())
 
-    mongo();
+  mongo();
 
-    const httpServer = http.createServer(app);
-    const typeDefs = fs.readFileSync(path.join(__dirname, './schema.graphql')).toString('utf-8')
-    const schema = makeExecutableSchema({ typeDefs, resolvers })
-    const pubsub = new PubSub()
+  const httpServer = http.createServer(app);
+  const typeDefs = fs.readFileSync(path.join(__dirname, './schema.graphql')).toString('utf-8')
+  // const schema = makeExecutableSchema({ typeDefs, resolvers })
+  const schema = await buildSchema({
+    resolvers: [ColorResolver],
+    // automatically create `schema.gql` file with schema definition in current folder
+    emitSchemaFile: path.resolve(__dirname, "schema.gql"),
+  });
+  const pubsub = new PubSub()
 
-    const subscriptionBuildOptions = async (connectionParams: any, webSocket: any) => { 
+  const subscriptionBuildOptions = async (connectionParams: any, webSocket: any) => {
     try {
       const { name, userID } = connectionParams
       if (!userID || !name) throw new Error("UserID and name must be filled.")
-      const user = await db.User.findOne({name, userID})
-      if (user){
+      const user = await db.User.findOne({ name, userID })
+      if (user) {
         return { db, userID, pubsub };
-      }else{
+      } else {
         throw new Error("User not found.")
       }
-    } catch (e) {}
-  } 
+    } catch (e) { }
+  }
 
   const subscriptionDestroyOptions = async (webSocket: any, context: any) => {
     const initialContext = await context.initPromise
-    if (initialContext){
-      const {userID} = initialContext;
+    if (initialContext) {
+      const { userID } = initialContext;
       // TODO: delete this user from editing
-      await db.User.deleteOne({userID})
+      await db.User.deleteOne({ userID })
     }
   }
 
   const subscriptionServer = SubscriptionServer.create(
-    { 
-      schema, 
-      execute, 
-      subscribe ,
+    {
+      schema,
+      execute,
+      subscribe,
       onConnect: subscriptionBuildOptions,
       onDisconnect: subscriptionDestroyOptions
     },
     { server: httpServer, path: '/graphql' }
   );
- 
+
 
   const server = new ApolloServer({
     schema,
-    context: async({req}) => {
+    context: async ({ req }) => {
       try {
         const { name, userid } = req.headers
         if (!userid || !name) throw new Error("UserID and name must be filled.")
-        const user = await db.User.findOne({name, userID: userid})  
-        if (!user){
-          const newUser = await new db.User({name, userID: userid}).save()
+        const user = await db.User.findOne({ name, userID: userid })
+        if (!user) {
+          const newUser = await new db.User({ name, userID: userid }).save()
         }
         return { db, userID: userid, pubsub }
-      } catch (e) {}
+      } catch (e) { }
     },
     plugins: [
       {
